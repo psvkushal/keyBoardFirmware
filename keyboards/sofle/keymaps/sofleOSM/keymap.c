c3ae4daa107f96ecaf627939a1921a42f722e382
psvkushal <psvkushal@gmail.com>
2024-06-04 19:58:21 +0530

    Makes a lot of change!!
    Moves the mods on number layer to the bottom row
    Though I am still thinking if may be keeping them in the middle row
    with the numbers as top row might be a better idea,
    
    Thing against keeping number row in top row is that, there is no actual
    advantage of keeping mods in the home row in num layer other than
    getting it same with the multimedia layer, again there are different
    ways to look at it and all the modifiers are available in home row
    
    Adds a workspace layer to move between the workspaces, part of me got
    irritated when using top row mods and trying to move between the layers
    I will try to move between the workspaces now with bottom row to check
    how it will go, for now I will leave it at that
    
    The tap dance is such a nice feature, it can be used to group together
    keys based on their meaning or usage inorder to decrease the usage. I
    think I can use the tap dance to Capitalise the words lets think of it
    later
    
    Started using combos for tab and enter which seems to be a very good
    idea, also used the saved up thumb key for the purpose of ctrl
    
    The only not nice part of the keyboard right now is the alt key present
    on the left pinkie, which kinda feels wrong, I kept it with the
    anticipation of usage with workspaces with yabai or hyprland, but this
    feels like premature optimization at this point, and most of the
    workspace shortcuts I want seems to be already possible in the workspace
    layer or the navigation layer, though I will keep it there for now
    
    One thing I would like to try with that alt key would be decrease one
    shot duration from 5s to 1s may be? or try implementing callum one shot
    mods copying the code from dallius blog
    
    I wish I could create a gaming layer but at this point it feels like too
    much for my mind to keep up with and ingeneral I am not playing any
    games right now to warrant spending time on it, and it feels better to
    buy an xbox controller right now
    
    Would like to add unicode mainly for using emojis in neovim, I thinking
    solving this in neovim software level might be better idea than going to
    the firmware level, other place I would like emojis would be at twitter
    but yeah I dont want to work on emojis for that purpose alone, but
    creating an extension to detect when you are trying to add emojis is a
    good idea

--- keyboards/sofle/keymaps/sofleOSM/keymap.c
+++ keyboards/sofle/keymaps/sofleOSM/keymap.c
@@ -11,11 +11,29 @@ enum sofle_layers {
     _SYMBOL
 };
 
+enum custom_keycodes {
+    OSM_CLR
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case OSM_CLR:
+            if (record->event.pressed) {
+                // Do something when pressed
+                clear_oneshot_mods();
+            }
+            return false; // Skip all further processing of this key
+        default:
+            return true;
+    }
+}
+
 const uint16_t PROGMEM jk_combo[] = {KC_J, KC_K, COMBO_END};
 const uint16_t PROGMEM kl_combo[] = {KC_K, KC_L, COMBO_END};
 const uint16_t PROGMEM ui_combo[] = {KC_U, KC_I, COMBO_END};
 const uint16_t PROGMEM io_combo[] = {KC_I, KC_O, COMBO_END};
 const uint16_t PROGMEM df_combo[] = {KC_D, KC_F, COMBO_END};
+const uint16_t PROGMEM sd_combo[] = {KC_S, KC_D, COMBO_END};
 const uint16_t PROGMEM mcomma_combo[] = {KC_M, KC_COMM, COMBO_END};
 
 combo_t key_combos[] = {
@@ -34,16 +52,19 @@ combo_t key_combos[] = {
 
 //Tap Dance Declarations
 enum {
-  TD_WS0 = 0,
-  TD_WS1 = 1,
-  TD_WS2 = 2,
-  TD_WS3 = 3,
-  TD_WS4 = 4,
-  TD_WS5 = 5,
-  TD_WS6 = 6,
-  TD_WS7 = 7,
-  TD_WS8 = 8,
-  TD_WS9 = 9
+    TD_WS0 = 0,
+    TD_WS1 = 1,
+    TD_WS2 = 2,
+    TD_WS3 = 3,
+    TD_WS4 = 4,
+    TD_WS5 = 5,
+    TD_WS6 = 6,
+    TD_WS7 = 7,
+    TD_WS8 = 8,
+    TD_WS9 = 9,
+    TD_SCR_UP = 10,
+    TD_SCR_DN = 11
+
 };
 
 //Tap Dance Definitions
@@ -58,7 +79,9 @@ tap_dance_action_t tap_dance_actions[] = {
   [TD_WS6]  = ACTION_TAP_DANCE_DOUBLE(A(KC_6), LSA(KC_6)),
   [TD_WS7]  = ACTION_TAP_DANCE_DOUBLE(A(KC_7), LSA(KC_7)),
   [TD_WS8]  = ACTION_TAP_DANCE_DOUBLE(A(KC_8), LSA(KC_8)),
-  [TD_WS9]  = ACTION_TAP_DANCE_DOUBLE(A(KC_9), LSA(KC_9))
+  [TD_WS9]  = ACTION_TAP_DANCE_DOUBLE(A(KC_9), LSA(KC_9)),
+  [TD_SCR_UP]  = ACTION_TAP_DANCE_DOUBLE(KC_PGUP, KC_HOME),
+  [TD_SCR_DN]  = ACTION_TAP_DANCE_DOUBLE(KC_PGDN, KC_END)
 
 // Other declarations would go here, separated by commas, if you have them
 };
@@ -68,30 +91,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BASE] = LAYOUT(
   XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                         XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
   XXXXXXX,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                            KC_Y,   KC_U,   KC_I,   KC_O,   KC_P, KC_GRV,
-  OSM(MOD_LCTL),   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                            KC_H,   KC_J,   KC_K,   KC_L,KC_SCLN,KC_QUOT,
+  OSM(MOD_LALT),   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                            KC_H,   KC_J,   KC_K,   KC_L,KC_SCLN,KC_QUOT,
   XXXXXXX,   KC_Z,   KC_X,   KC_C,   KC_V,   KC_B, XXXXXXX,        XXXXXXX,   KC_N,   KC_M,KC_COMM, KC_DOT,KC_SLSH, KC_BSPC,
-                 XXXXXXX,XXXXXXX,KC_LGUI, TL_LOWR,  KC_ENT,         KC_SPC,TL_UPPR,KC_LSFT,XXXXXXX,XXXXXXX
+                 XXXXXXX,XXXXXXX,KC_LGUI, TL_LOWR,  KC_LCTL,         KC_SPC,TL_UPPR,KC_LSFT,XXXXXXX,XXXXXXX
 ),
 [_NUM] = LAYOUT(
   XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                     XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
-  _______, OSM(MOD_LSFT), OSM(MOD_LCTL), OSM(MOD_LALT), OSM(MOD_LGUI), _______,                    KC_PPLS ,  KC_PIPE, KC_EQL,  _______, _______,  _______,
-  _______,   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  _______,
-  _______,_______,_______,KC_LBRC,KC_RBRC, _______, _______,       _______, KC_MINS, KC_UNDS, _______, _______, KC_BSLS, _______,
+  _______,_______,_______,    KC_LBRC,KC_LPRN,  KC_TILD,                     KC_PPLS ,KC_RPRN, KC_RBRC ,  KC_EQL,KC_PIPE, _______,
+  _______,   KC_1,   KC_2,   KC_3,   KC_4,   KC_5,                       KC_6,   KC_7,KC_8,    KC_9,   KC_0,_______,
+  _______, OSM(MOD_LSFT), OSM(MOD_LCTL), OSM(MOD_LALT), OSM(MOD_LGUI), OSM_CLR,_______,                  _______, KC_MINS, KC_UNDS, _______, _______, KC_BSLS, _______,
                        _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
 ),
+
     // The intention of KC_RGUI is to act as mod for moving between workspaces
 [_SYMBOL] = LAYOUT(
 XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                                          XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
  _______,_______, _______, _______, _______, _______,                                _______, _______, _______, _______, _______, _______,
- _______,TD(TD_WS1), TD(TD_WS2), TD(TD_WS3), TD(TD_WS4), TD(TD_WS5),                                   TD(TD_WS6), TD(TD_WS7), TD(TD_WS8), TD(TD_WS9), TD(TD_WS0), _______,
+ _______,TD(TD_WS1), TD(TD_WS2), TD(TD_WS3), TD(TD_WS4), TD(TD_WS5),                                   TD(TD_WS6), TD(TD_WS8), TD(TD_WS9), TD(TD_WS0),  TD(TD_WS7),_______,
  _______, _______, _______, _______, _______, _______,  _______,              _______, _______, _______, _______, _______, _______, _______,
                        _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
 ),
 [_UTIL] = LAYOUT(
   XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                                                      XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
-      _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, _______,                                             KC_PGUP, KC_F4, KC_UP , KC_BRIU,  KC_VOLU, _______,
-      _______, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, _______,                                                         KC_TAB, KC_LEFT, KC_DOWN, KC_RIGHT, KC_MUTE,  _______,
-      _______, _______, _______, _______, KC_F12, KC_F5,  _______,                           _______,KC_PGDN, KC_F6,KC_F2 ,  KC_BRID,  KC_VOLD,_______,
+      KC_F10, _______, KC_MPRV, KC_MPLY, KC_MNXT, _______,                                            TD(TD_SCR_UP), KC_F4, KC_UP , KC_BRIU,  KC_VOLU,_______,
+      KC_F11, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, _______,                                                         KC_TAB, KC_LEFT, KC_DOWN, KC_RIGHT, KC_MUTE,  _______,
+      KC_F9, KC_F1, KC_F3, KC_F7, KC_F12, KC_F5,  KC_F8,                           _______,TD(TD_SCR_DN), KC_F6,KC_F2 ,  KC_BRID,  KC_VOLD,_______,
                          _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
 )
 };
@@ -133,7 +157,7 @@ void print_layer_status(void) {
             oled_write_P(PSTR("Prod"), false);
             break;
         case 3:
-            oled_write_ln_P(PSTR("Syms"), false);
+            oled_write_ln_P(PSTR("WS"), false);
             break;
         case 4:
             oled_write_ln_P(PSTR("Utils"), false);
